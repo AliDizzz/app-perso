d4627580b7c90025da901021c6511b42064adc59
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-01 14:06:18 +0100

    Register

--- src/components/pages/users/Register.jsx
+++ src/components/pages/users/Register.jsx
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className="tab-content">
+            <form action="/" className="form-profile">
+                <fieldset>
+                    <legend>S'enregistrer'</legend>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="text"
+                        name="username"
+                        className="form-control"
+                        id="email"
+                        placeholder="mail@mail.com"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="password"
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-outline-primary">
+                        Save
+                    </button>
+                </fieldset>
+            </form>
         </div>
     );
 };
