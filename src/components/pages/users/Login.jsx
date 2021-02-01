8feb240cf2978f0d147bdf8211bf57f8adfc84c7
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-01 14:04:21 +0100

    Login

--- src/components/pages/users/Login.jsx
+++ src/components/pages/users/Login.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const Login = () => {
+const Login = (props) => {
     return (
-        <div>
-            Login
+        <div className="tab-content">
+            <form action="/" className="form-profile">
+                <fieldset>
+                    <legend>Se connecter</legend>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                        type="text"
+                        name="username"
+                        className="form-control"
+                        id="email"
+                        placeholder="mail@mail.com"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="password"
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-outline-primary">
+                        Se connecter
+                    </button>
+                </fieldset>
+            </form>
         </div>
     );
 };
