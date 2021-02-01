411a2bdc14f62716d574c65f0dc27201818e1380
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-01 13:51:21 +0100

    posts content

--- src/components/pages/posts/Posts.jsx
+++ src/components/pages/posts/Posts.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 const Posts = () => {
     return (
-        <div>
-            Posts
+        <div className="card-container">
+            <div className="card border-info mb-3">
+                <div className="card-header">Post name</div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Post Content
+                    </p>
+                </div>
+            </div>
+
+            <div className="card border-info mb-3">
+                <div className="card-header">Post name</div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Post Content
+                    </p>
+                </div>
+            </div>
+
+            <div className="card border-info mb-3">
+                <div className="card-header">Post name</div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Post Content
+                    </p>
+                </div>
+            </div>
+
+            <div className="card border-info mb-3">
+                <div className="card-header">Post name</div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Post Content
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
