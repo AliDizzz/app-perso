f898c99df46d3f6196b819d69f6b3742eba9d9b5
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-02 13:27:50 +0100

    Acount

--- src/components/pages/users/Account.jsx
+++ src/components/pages/users/Account.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 
 const Account = (props) => {
-    const [currentTab, setCurrentTab] = useState({name: "profile"})
+    
+    const [currentTab, setCurrentTab] = useState({name: "profile"});
     const [tabs] = useState([
         { tabKey: 'profile', tabTitle: 'Mon profile'},
         { tabKey: 'posts', tabTitle: 'Mes articles'},
-    ])
+    ]);
+    const handleTabs = (name) => {
+        setCurrentTab({ name });
+    };
+
     return (
         <>
         <div className="tabs">
             <ul>
-                {tabs.map((tab, index) => 
-                <li className={`tabs-pane ${currentTab.name === tab.tabKey ? "active" : ""}`}>
-                    <span onClick={() => }></span>
+                {tabs.map((tab, index) => (
+                    <li className={`tabs-pane ${currentTab.name === tab.tabKey ? "active" : ""}`}>
+                    <span onClick={() => handlTabs(tab.tabKey)}>{tab.tabTitle}</span>
                 </li>
+                )
                 )}
             </ul>
         </div>
