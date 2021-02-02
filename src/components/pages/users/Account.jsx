eb7ef3849cbf9a63f12ca479540fd5800179a85d
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-02 13:22:02 +0100

    Acount

--- src/components/pages/users/Account.jsx
+++ src/components/pages/users/Account.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Account = (props) => {
+    const [currentTab, setCurrentTab] = useState({name: "profile"})
+    const [tabs] = useState([
+        { tabKey: 'profile', tabTitle: 'Mon profile'},
+        { tabKey: 'posts', tabTitle: 'Mes articles'},
+    ])
     return (
-        <div>
-            Account
+        <>
+        <div className="tabs">
+            <ul>
+                {tabs.map((tab, index) => 
+                <li className={`tabs-pane ${currentTab.name === tab.tabKey ? "active" : ""}`}>
+                    <span onClick={() => }></span>
+                </li>
+                )}
+            </ul>
         </div>
+        </>
     );
 };
 
