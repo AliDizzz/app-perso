0266d616d51fe6830d6d3832ed215f282956cb61
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-02 13:04:58 +0100

    Profile

--- src/components/pages/users/Profile.jsx
+++ src/components/pages/users/Profile.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Profile = () => {
+const Profile = (props) => {
     return (
-        <div>
-            Profile
+        <div className="tab-content">
+            <div className="">
+                <div className="card border-info mb-3">
+                    <div className="card-header">
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                Hello <span>Ali</span>
+                            </h4>
+                            <p className="card-text">date de création: ....</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/pages/users/Register.jsx
+++ src/components/pages/users/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Register = () => {
+const Register = (props) => {
     return (
         <div className="tab-content">
             <form action="/" className="form-profile">
