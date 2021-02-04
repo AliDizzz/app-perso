e214f5b277259e3666f044aff04c28a31dedc965
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-04 09:16:07 +0100

    navigation portfolio

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,26 @@ const Navigation = () => {
     return (
         <div className="navigation">
             <ul>
-                <NavLink 
-                to="/" 
-                exact className="hover" 
-                activeClassName="nav-active"
-                >
+                <NavLink to="/" exact className="hover" activeClassName="nav-active">
                     <li>Accueil</li>
                 </NavLink>
+                
+                <li className="nav-portfolio">Portfolio
+                    <ul className="nav-projects">
+                        <NavLink to="/project-1" activeClassName="nav-active" className="hover">
+                            <li>project 1</li>
+                        </NavLink>
+                        <NavLink to="/project-2" activeClassName="nav-active" className="hover">
+                            <li>project 2</li>
+                        </NavLink>
+                        <NavLink to="/project-3" activeClassName="nav-active" className="hover">
+                            <li>project 3</li>
+                        </NavLink>
+                        <NavLink to="/project-4" activeClassName="nav-active" className="hover">
+                            <li>project 4</li>
+                        </NavLink>
+                    </ul>
+                </li>
             </ul>
         </div>
     );
