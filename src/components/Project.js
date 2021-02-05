46e8169529ed374e7bf4e7ce150dfe8184639621
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-05 11:55:35 +0100

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,6 +16,20 @@ const Project = (props) => {
                     })}
                 </ul>
             </div>
+            <div className="img-content">
+                <div className="img-container hover">
+                    <span>
+                        <h3>{project.title}</h3>
+                        <p>{project.infos}</p>
+                    </span>
+                    <img src={project.img} alt={project.title}/>
+                </div>
+                <div className="button-container">
+                    <a href={project.link} target="_blank" rel="noopener noreferrer" className="hover">
+                        <span className="button">voir le site</span>
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
