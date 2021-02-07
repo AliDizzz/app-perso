5ef51488ed6ffdf5ceb7b1e407e428d25e09fe2b
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-07 22:09:21 +0100

    mouse

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -6,16 +6,14 @@ const Mouse = () => {
     useEffect(() => {
         
         const handleCursor = (e) => {
-            
+            document.querySelector('.cursor').style.top = e.pageY + "px";
+            document.querySelector('.cursor').style.left = e.pageX + "px";
         }
 
         window.addEventListener('mousemove', handleCursor)
-    }, [input]);
+    }, []);
 
-    return (
-    
-        <span className="cursor"></span>
-    );
+    return <span className="cursor"></span>;
 };
 
 export default Mouse;
