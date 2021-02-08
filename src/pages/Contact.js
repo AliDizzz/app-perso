b6c45ee5aa06d2ba3a40faea8900f41e43f988dd
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-08 09:36:24 +0100

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -26,7 +26,16 @@ const Contact = () => {
                         <h4>téléphone</h4>
                         <CopyToClipboard text="0773484244" className="hover">
                             <p style={{cursor: 'pointer'}} className="clipboard" onClick={() => {
-                                alert("Téléphone copié !");                            }}>07 73 48 42 44</p>
+                                alert("Téléphone copié !");}}>07 73 48 42 44</p>
+                        </CopyToClipboard>
+                    </div>
+                </div>
+                <div className="email">
+                    <div className="content">
+                        <h4>email</h4>
+                        <CopyToClipboard text="yildiz.ali.s1990@gmail.com" className="hover">
+                        <p style={{cursor: 'pointer'}} className="clipboard" onClick={() => {
+                                alert("Email copié !");}}>yildiz.ali.s1990@gmail.com</p>
                         </CopyToClipboard>
                     </div>
                 </div>
