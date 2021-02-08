762cc6c7542c4a9540ea18b1c10827c046d5e92d
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-08 09:15:24 +0100

    mouse

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -10,7 +10,22 @@ const Mouse = () => {
             document.querySelector('.cursor').style.left = e.pageX + "px";
         }
 
-        window.addEventListener('mousemove', handleCursor)
+        window.addEventListener('mousemove', handleCursor);
+
+        const handleHover = () => {
+            document.querySelector('.cursor').classList.add('hovered');
+        };
+
+        const handleLeave = () => {
+            document.querySelector('.cursor').style.transition = ".3s ease-out";
+            document.querySelector('.cursor').classList.remove('hovered');
+        }
+
+        document.querySelectorAll('.hover').forEach((link) => {
+            link.addEventListener('mouseover', handleHover);
+            link.addEventListener('mouseleave', handleLeave);
+        })
+
     }, []);
 
     return <span className="cursor"></span>;
