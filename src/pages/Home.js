c1dcbad4fc6e9e5afa43251d06e6a7c03f816480
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-12 10:49:44 +0100

    Home update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
-import { motion } from 'framer-motion';
-import React from 'react';
-import ButtonsBottom from '../components/ButtonsBottom';
-import DynamicText from '../components/DynamicText';
-import Mouse from '../components/Mouse';
-import Navigation from '../components/Navigation';
-import SocialNetwork from '../components/SocialNetwork';
+import React from "react";
+import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
+import ButtonsBottom from "../components/ButtonsBottom";
+import DynamicText from "../components/DynamicText";
+import Mouse from "../components/Mouse";
+import { motion } from "framer-motion";
 
 const Home = () => {
 
@@ -16,33 +16,38 @@ const Home = () => {
         },
         visible: {
             opacity: 1,
-            x: 0
+            x: 0,
         },
         exit: {
             opacity: 0,
             transition: { duration: 0.3 },
-            x: -100
+            x: -100,
         }
-    }
-    
+    };
+
     return (
         <main>
             <Mouse />
             <motion.div
                 className="home"
                 initial="initial"
-                animate="animate"
+                animate="visible"
                 exit="exit"
-                variants={variants}>
+                variants={variants}
+            >
                 <Navigation />
                 <SocialNetwork />
                 <div className="home-main">
                     <div className="main-content">
-                        <h1>CAFE RACER</h1>
-                        <h2> <DynamicText /> </h2>
+                        <motion.h1 drag onDragEnd dragConstraints={{ left: -250, right: 950, top: -200, bottom: 250 }}>
+                            CAFE RACER
+            </motion.h1>
+                        <motion.h2 drag onDragEnd dragConstraints={{ left: -250, right: 1250, top: -200, bottom: 250 }}>
+                            <DynamicText />
+                        </motion.h2>
                     </div>
                 </div>
-                <ButtonsBottom right={"/project-1"} />
+                <ButtonsBottom right={"/projet-1"} />
             </motion.div>
         </main>
     );
